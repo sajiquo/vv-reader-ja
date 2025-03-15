5f08ad9c83e3b50f7a5511a3a9409dbb941e167e
sajiquo <97267747+sajiquo@users.noreply.github.com>
2025-03-15 16:05:00 +0900

    setup (#1)
    * chore: migrate to biome from eslint
    
    * chore: apply biome fix
    
    * chore: add orval
    
    * chore: scaffold speaker
    
    * style: fixes and renames

---
+++ orval.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "orval";
+
+export default defineConfig({
+	petstore: {
+		input: "./openapi.json",
+		output: {
+			mode: "split",
+			client: "fetch",
+			target: "src/gen/endpoints",
+			schemas: "src/gen/models",
+			mock: true,
+			baseUrl: "http://127.0.0.1:50021",
+		},
+	},
+});
